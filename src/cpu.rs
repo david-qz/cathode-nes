5edb8afc13f8cb7d2341d185c717b492fb779548
David Quennoz <105565781+david-qz@users.noreply.github.com>
2023-03-03 22:07:47 -0800

    Initilize program counter correctly

--- src/cpu.rs
+++ src/cpu.rs
@@ -16,16 +16,20 @@ pub struct CPU {
     brk_command: bool,
     overflow: bool,
     negative: bool,
+
+    should_run_reset_procedure: bool,
 }
 
 impl CPU {
+    const RESET_VECTOR: u16 = 0xFFFA;
+
     pub fn new() -> Self {
         Self {
             a: 0,
             x: 0,
             y: 0,
-            pc: 0, // TODO: read reset vector and set program counter appropriately
-            s: 0,  // TODO: determine where the stack pointer should be initialized
+            pc: 0,
+            s: 0, // TODO: determine where the stack pointer should be initialized
             carry: false,
             zero: false,
             irq_disable: false,
@@ -33,10 +37,16 @@ impl CPU {
             brk_command: false,
             overflow: false,
             negative: false,
+            should_run_reset_procedure: true,
         }
     }
 
     pub fn clock(&mut self, bus: &mut dyn Bus16) {
+        if self.should_run_reset_procedure {
+            self.pc = bus.read_word(Self::RESET_VECTOR);
+            return;
+        }
+
         let opcode = bus.read_byte(self.pc);
 
         match opcode {
